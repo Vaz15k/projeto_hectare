75aa506a21386eef81cd5e66e1f3c6245dec247f
Vaz15K <keveneduardobilibio@hotmail.com>
2026-07-04 09:19:35 -0400

    feat: Implementação Caddy
    - Parece simples, mais isso aqui foi incrivel pra min... Não tinha configurado nada para deploy...
    - Muito Feliz :)

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,5 +4,8 @@ set -eu
 echo "Aplicando migrações do banco de dados..."
 python manage.py migrate --noinput
 
+echo "Coletando arquivos estáticos..."
+python manage.py collectstatic --noinput
+
 echo "Iniciando aplicação..."
 exec "$@"
